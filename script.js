5d1c019d72457f59ce4f421b25516c39146980f2
Emmanuel Adebisi <emmisco07@gmail.com>
2024-04-26 07:03:25 +0100

    table

---
+++ script.js
@@ -0,0 +1,95 @@
+// Table 1
+let multi1 = ''
+let screen1 = document.getElementById('display1')
+for(let i = 1; i < 13; i++){
+    multi1 +="1 " + 'x ' + `${i}`+ " = " +   `${i * 1}` + '<br>'
+}
+screen1.innerHTML = multi1;
+
+//Table 2
+let multi2 = ''
+let screen2 = document.getElementById('display2')
+for(let i = 1; i < 13; i++){
+    multi2 +="2 " + 'x ' + `${i}`+ " = " +   `${i * 2}` + '<br>'
+}
+screen2.innerHTML = multi2;
+
+// Table 3
+let multi3 = ''
+let screen3 = document.getElementById('display3')
+for(let i = 1; i < 13; i++){
+    multi3 +="3 " + 'x ' + `${i}`+ " = " +   `${i * 3}` + '<br>'
+}
+screen3.innerHTML = multi3;
+
+// Table 4
+let multi4 = ''
+let screen4 = document.getElementById('display4')
+for(let i = 1; i < 13; i++){
+    multi4 +="4 " + 'x ' + `${i}`+ " = " +   `${i * 4}` + '<br>'
+}
+screen4.innerHTML = multi4;
+
+// Table 5
+let multi5 = ''
+let screen5 = document.getElementById('display5')
+for(let i = 1; i < 13; i++){
+    multi5 +="5 " + 'x ' + `${i}`+ " = " +   `${i * 5}` + '<br>'
+}
+screen5.innerHTML = multi5;
+
+// Table 6
+let multi6 = ''
+let screen6 = document.getElementById('display6')
+for(let i = 1; i < 13; i++){
+    multi6 +="6 " + 'x ' + `${i}`+ " = " +   `${i * 6}` + '<br>'
+}
+screen6.innerHTML = multi6;
+
+// Table 7
+let multi7 = ''
+let screen7 = document.getElementById('display7')
+for(let i = 1; i < 13; i++){
+    multi7 +="7 " + 'x ' + `${i}`+ " = " +   `${i * 7}` + '<br>'
+}
+screen7.innerHTML = multi7;
+
+// Table 8
+let multi8 = ''
+let screen8 = document.getElementById('display8')
+for(let i = 1; i < 13; i++){
+    multi8 +="8 " + 'x ' + `${i}`+ " = " +   `${i * 8}` + '<br>'
+}
+screen8.innerHTML = multi8;
+
+// Table 9
+let multi9 = ''
+let screen9 = document.getElementById('display9')
+for(let i = 1; i < 13; i++){
+    multi9 +="9 " + 'x ' + `${i}`+ " = " +   `${i * 9}` + '<br>'
+}
+screen9.innerHTML = multi9;
+
+// Table 10
+let multi10 = ''
+let screen10 = document.getElementById('display10')
+for(let i = 1; i < 13; i++){
+    multi10 +="10 " + 'x ' + `${i}`+ " = " +   `${i * 10}` + '<br>'
+}
+screen10.innerHTML = multi10;
+
+// Table 11
+let multi11 = ''
+let screen11 = document.getElementById('display11')
+for(let i = 1; i < 13; i++){
+    multi11 +="11 " + 'x ' + `${i}`+ " = " +   `${i * 11}` + '<br>'
+}
+screen11.innerHTML = multi11;
+
+// Table 12
+let multi12 = ''
+let screen12 = document.getElementById('display12')
+for(let i = 1; i < 13; i++){
+    multi12 +="12 " + 'x ' + `${i}`+ " = " +   `${i * 12}` + '<br>'
+}
+screen12.innerHTML = multi12;
